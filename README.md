6884e1ef9704286a6eadc42a2a3cef449d76540d
hernandezrivera <hernandez@reliefweb.int>
2019-06-07 15:13:18 -0400

    Running sequential version of the script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # lighthouse-multisite
 
+Script for parsing multiple sites and getting the basic Google Lighthouse indicators into a csv files.
+
 Uses Google's lighthouse (https://github.com/GoogleChrome/lighthouse) to build a set of reports from the URL list you pass into the configuration file and writes the results into one file.
 This project is a fork of this project https://github.com/sahava/multisite-lighthouse
 
@@ -17,11 +19,15 @@ In `config.json`, edit the following fields:
 
 | Field | Example | Description |
 |-------|---------|-------------|
-| `url` | `["https://www.google.com/", "https://www.simoahava.com/about-simo-ahava/"]` | Array with list of fully formatted URLs to audit. |
+| `url` | `["https://www.google.com/", "https://www.simoahava.com/about-simo-ahava/"]` | Array with list of fully formatted URLs to audit. It comes from the OCHA URLs to audit |
+
+# Configuration file explanation
+
+| Field | Example | Description |
+|-------|---------|-------------|
 | `lighthouseFlags` | `{"output": "csv", "disableDeviceEmulation": true}` | List of flags to pass to lighthouse. Full list available here: https://github.com/GoogleChrome/lighthouse/blob/8f500e00243e07ef0a80b39334bedcc8ddc8d3d0/typings/externs.d.ts#L52 |
 | `chromeFlags` | `["--headless"]` | List of flags to pass to the Chrome launcher. Full list available here: https://peter.sh/experiments/chromium-command-line-switches/ |
 | `writeTo` | `"/users/sahava/Desktop/"` | The path where to write the reports - the tool will create the path if it doesn't exist. Remember the trailing slash in the end. |
-| `sortByDate` | `true` | If `true`, stores the report in a folder structure of `writeTo`/year/month/url_1.csv, for example. If set to `false`, sorts by file, so `writeTo`/url_1/year/month/url_1.csv. |
 
 # Run
 
@@ -31,4 +37,24 @@ Once you've set it up, you can run the audit tool with
 
 The process will be logged into the console. 
 
-The reports will be written in the format you chose for the `output` key in the configuration, and they will be written in the folder structure you specified in the `writeTo` and `sortByDate` keys of the configuration file.
+The reports will be written in the format you chose for the `output` key in the configuration, and they will be written in the file you specified in the `writeTo`.
+The fields of the output file are:
+- Lighthouse version
+- Timestamp of the execution
+- URL being tested
+- URL redirected 
+- Response code
+- Performance Score (0 to 100)
+- Accessibility Score (0 to 100)
+- Best Practices (0 to 100)
+- SEO Score (0 to 100)
+- PWA Score (0 to 100)
+
+
+# Notes
+If the script fails, you will have many Chrome processes running in your computer.
+Run ``ASKKILL /IM chrome.exe /F`` in your command line on windows to kill all chrome pending processes (and existing ones! save your chrome work before doing this!).
+
+# TO-DO List
+- Read URLs directly from Google Sheet or published CSV from the Google Sheet
+- Write the results into the specified folder
--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 const fs = require("fs");
+const async = require("async");
 
 const configJson = JSON.parse(fs.readFileSync("config.json"));
-
-function launchChromeAndRunLighthouse(url, opts, config = null) {
-    return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
-        console.log("Launching lighthouse for " + url);
-        opts.lighthouseFlags.port = chrome.port;
-        return lighthouse(url, opts.lighthouseFlags, config).then(res => {
-            console.log("Parsing report for " + url);
-            return chrome.kill().then(() => res.report);
-        });
-    });
-}
 
 const file = configJson.filename;
 const today = new Date();
@@ -22,51 +12,74 @@ const folder = "./";
 const file_prefix = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate() + "-";
 const dest_file = folder + file_prefix + file;
 
-/*
-fs.access(dest_file, fs.constants.F_OK | fs.constants.W_OK, (err) => {
-  if (err) {
-	fs.appendFile(dest_file, "Lighthouse version, Time, URL, redirect URL, Response, PWA, PWE, ACC, BP, SEO", (err) => 
-	  {   if (err) { console.log(err); } } );
-}});
-*/
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+    return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+        console.log("Launching lighthouse for " + url);
+        opts.lighthouseFlags.port = chrome.port;
+        return lighthouse(url, opts.lighthouseFlags, config).then(res => {
+            console.log("Parsing report for " + url);
+            resultJson = JSON.parse(res.report);
+            if ((res.lhr.categories.performance.score === 0) && (res.lhr.runtimeError.code === "NO_ERROR"))
+            {
+                console.log(url + " run successfully with 0 performance, rerunning lighthouse");
+                launchChromeAndRunLighthouse(url, configJson)
+                    .catch(err => {
+                        console.log(err)
+                    });
+            } else {
+                writeResults(folder, dest_file, url, resultJson);
+            }
+            chrome.kill().catch(err => { console.log(err) });
+        });
+    });
+}
+
+function writeResults (folder, dest_file, address, resultJson) {
+    console.log("Writing analysis to " + dest_file);
+    fs.mkdir(folder, {recursive: true}, (err) => {
+        if (!err) {
+            resultString = resultJson.lighthouseVersion + ", " +
+                resultJson.fetchTime + ", " +
+                address + ", " +
+                resultJson.finalUrl + ", " +
+                resultJson.runtimeError.code + ", " +
+                resultJson.categories.performance.score * 100 + ", " +
+                resultJson.categories.accessibility.score * 100 + ", " +
+                resultJson.categories["best-practices"].score * 100 + ", " +
+                resultJson.categories.seo.score * 100 + ", " +
+                resultJson.categories.pwa.score * 100 + "\n";
+            // if file doesn't exist then write headers
+
+            fs.appendFile(dest_file, resultString, (err) => {
+
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Analysis saved to " + dest_file);
+                    console.log(resultString);
+                }
+
+            });
+        } else {
+            console.log(err);
+        }
+    });
+
+}
 
 let resultJson;
 let resultString;
 
-configJson.url.forEach(address => {
+last = configJson.urls_to_analyze + configJson.offset;
+if (configJson.urls_to_analyze + configJson.offset > configJson.url.length ) { last = configJson.url.length; }
+
+console.log("Analyzing " + configJson.url.length + " urls");
+
+//for (let i = configJson.offset; i < last ; i++) {
+async.eachSeries(configJson.url, function (address, next) {
+    //address = configJson.url[i];
     console.log("Starting analysis on " + address);
-    launchChromeAndRunLighthouse(address, configJson).then(results => {
-
-        console.log("Writing analysis to " + dest_file);
-        fs.mkdir(folder, {recursive: true}, (err) => {
-            if (!err) {
-                resultJson = JSON.parse(results);
-
-                resultString = resultJson.lighthouseVersion + ", " +
-                    resultJson.fetchTime + ", " +
-                    resultJson.requestedUrl + ", " +
-                    resultJson.finalUrl + ", " +
-                    resultJson.runtimeError.code + ", " +
-                    resultJson.categories.performance.score * 100 + ", " +
-                    resultJson.categories.accessibility.score * 100 + ", " +
-                    resultJson.categories["best-practices"].score * 100 + ", " +
-                    resultJson.categories.seo.score * 100 + ", " +
-                    resultJson.categories.pwa.score * 100 + "\n";
-                // if file doesn't exist then write headers
-
-                fs.appendFile(dest_file, resultString, (err) => {
-
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Analysis saved to " + dest_file);
-                        console.log(resultString);
-                    }
-
-                });
-            } else {
-                console.log(err);
-            }
-        });
-    }).catch(err => console.log(err));
+    launchChromeAndRunLighthouse(address, configJson)
+        .then(results => {next();})
+        .catch(err => { console.log(err)});
 });
